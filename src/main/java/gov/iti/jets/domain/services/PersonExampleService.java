17f87d657fb4c888e8d7f3bad1f1385c51f6dc58
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-03-22 19:20:05 +0200

    Create package structure

---
+++ src/main/java/gov/iti/jets/domain/services/PersonExampleService.java
@@ -0,0 +1,4 @@
+package gov.iti.jets.domain.services;
+
+public class PersonExampleService {
+}
---
+++ src/main/java/gov/iti/jets/presentation/controllers/ExampleAjaxServlet.java
@@ -0,0 +1,4 @@
+package gov.iti.jets.presentation.controllers;
+
+public class ExampleAjaxServlet {
+}
