d1f259ccba65e350697ccac110826b477ee9e42c
Mahmoud Fawzy <mahmoudfawzykhalil98@gmail.com>
2022-03-24 23:06:21 +0200

    Have findOne return an Optional

--- src/main/java/gov/iti/jets/repository/AbstractRepository.java
+++ src/main/java/gov/iti/jets/repository/AbstractRepository.java
@@ -3,43 +3,44 @@ package gov.iti.jets.repository;
 import jakarta.persistence.EntityManager;
 
 import java.util.List;
+import java.util.Optional;
 
 public abstract class AbstractRepository<T> {
     protected Class<T> clazz;
 
-    protected void setClazz(Class<T> clazz) {
+    protected void setClazz( Class<T> clazz ) {
         this.clazz = clazz;
     }
 
     private final EntityManager entityManager;
 
-    protected AbstractRepository(EntityManager entityManager) {
+    protected AbstractRepository( EntityManager entityManager ) {
         this.entityManager = entityManager;
     }
 
-    public T findOne(long id) {
-        return entityManager.find(clazz, id);
+    public Optional<T> findOne( int id ) {
+        return Optional.ofNullable( entityManager.find( clazz, id ) );
     }
 
     public List<T> findAll() {
-        return entityManager.createQuery("from " + clazz.getName(), clazz)
+        return entityManager.createQuery( "from " + clazz.getName(), clazz )
                 .getResultList();
     }
 
-    public void create(T entity) {
-        entityManager.persist(entity);
+    public void create( T entity ) {
+        entityManager.persist( entity );
     }
 
-    public T update(T entity) {
-        return entityManager.merge(entity);
+    public T update( T entity ) {
+        return entityManager.merge( entity );
     }
 
-    public void delete(T entity) {
-        entityManager.remove(entity);
+    public void delete( T entity ) {
+        entityManager.remove( entity );
     }
 
-    public void deleteById(long entityId) {
-        T entity = findOne(entityId);
-        delete(entity);
+    public void deleteById( int entityId ) {
+        T entity = entityManager.find( clazz, entityId );
+        delete( entity );
     }
 }
--- src/main/java/gov/iti/jets/repository/util/TestDb.java
+++ src/main/java/gov/iti/jets/repository/util/TestDb.java
@@ -14,70 +14,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestDb {
-    public static void main(String[] args) {
-        var emf = Persistence.createEntityManagerFactory("ecommerce");
+    public static void main( String[] args ) {
+        var emf = Persistence.createEntityManagerFactory( "ecommerce" );
         var em = emf.createEntityManager();
 
 
         // CREATE PRODUCTS ************************
         List<Product> products = new ArrayList<>();
-        Product product1 = new Product("product1","first product", "", 200,50, Category.CHOCOLATE);
-        products.add(product1);
+        Product product1 = new Product( "product1", "first product", "", 200, 50, Category.CHOCOLATE );
+        products.add( product1 );
 
-        Product product2 = new Product("product2","second product", "", 5000,10, Category.CHOCOLATE);
-        products.add(product2);
+        Product product2 = new Product( "product2", "second product", "", 5000, 10, Category.CHOCOLATE );
+        products.add( product2 );
 
 
         // PERSIST PRODUCTS ************************
-        ProductRepository pr = new ProductRepository(em);
+        ProductRepository pr = new ProductRepository( em );
         em.getTransaction().begin();
-        products.forEach(pr::create);
+        products.forEach( pr::create );
         em.getTransaction().commit();
 
         // CREATE USERS ************************
         List<User> users = new ArrayList<>();
-        User user1 = new User("John", "Doe", "john@mail.com","password", LocalDate.ofYearDay(2020,50), 6000, "salesman", Role.CUSTOMER, new Address("iti", "Cairo"));
-        users.add(user1);
+        User user1 = new User( "John", "Doe", "john@mail.com", "password", LocalDate.ofYearDay( 2020, 50 ), 6000, "salesman", Role.CUSTOMER, new Address( "iti", "Cairo" ) );
+        users.add( user1 );
 
         // PERSIST USERS ************************
-        UserRepository ur = new UserRepository(em);
+        UserRepository ur = new UserRepository( em );
         em.getTransaction().begin();
-        users.forEach(ur::create);
+        users.forEach( ur::create );
         em.getTransaction().commit();
 
         // CREATE SHOPPING CARTS ************************
         List<ShoppingCart> shoppingCarts = new ArrayList<>();
         ShoppingCart shoppingCart1 = new ShoppingCart();
-        user1.setShoppingCart(shoppingCart1);
+        user1.setShoppingCart( shoppingCart1 );
 
-        shoppingCarts.add(shoppingCart1);
+        shoppingCarts.add( shoppingCart1 );
 
-        shoppingCart1.addCartLineItem( new CartLineItem(product1,5, product1.getPrice()) );
-        shoppingCart1.addCartLineItem( new CartLineItem(product2,3, product2.getPrice()) );
+        shoppingCart1.addCartLineItem( new CartLineItem( product1, 5, product1.getPrice() ) );
+        shoppingCart1.addCartLineItem( new CartLineItem( product2, 3, product2.getPrice() ) );
 
         // TEST UPDATE QUANTITY ************************
-        shoppingCart1.updateCartLineItemProductQuantity(product1.getId(), 100);
+        shoppingCart1.updateCartLineItemProductQuantity( product1.getId(), 100 );
 
         // PERSIST SHOPPING CARTS ************************
-        ShoppingCartRepository scr = new ShoppingCartRepository(em);
+        ShoppingCartRepository scr = new ShoppingCartRepository( em );
         em.getTransaction().begin();
-        shoppingCarts.forEach(scr::create);
+        shoppingCarts.forEach( scr::create );
         em.getTransaction().commit();
 
         // CREATE ORDERS ************************
         List<Order> orders = new ArrayList<>();
-        Order order1 = new Order(user1);
-        order1.populateLineItemsFromCart(shoppingCart1);
-        orders.add(order1);
+        Order order1 = new Order( user1 );
+        order1.populateLineItemsFromCart( shoppingCart1 );
+        orders.add( order1 );
 
         // PERSIST ORDERS ************************
-        OrderRepository or = new OrderRepository(em);
+        OrderRepository or = new OrderRepository( em );
         em.getTransaction().begin();
-        orders.forEach(or::create);
+        orders.forEach( or::create );
         em.getTransaction().commit();
 
-        System.out.println(emf.createEntityManager().find(Address.class, 4));
-
-        System.out.println(new OrderRepository(em).findAll());
+        System.out.println( emf.createEntityManager().find( Address.class, 4 ) );
+        System.out.println( new UserRepository( em ).findOne( 3 ).get() );
+        System.out.println( new OrderRepository( em ).findAll() );
     }
 }
