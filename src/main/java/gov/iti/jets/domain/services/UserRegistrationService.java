e808db9feb8b587a210ee52e0994e6bcfd5339ca
Mariem <mariem.mfe@gmail.com>
2022-03-28 13:59:59 +0200

    Hash passwords in registration

--- src/main/java/gov/iti/jets/domain/services/UserRegistrationService.java
+++ src/main/java/gov/iti/jets/domain/services/UserRegistrationService.java
@@ -6,17 +6,30 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserRegistrationService {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory( "ecommerce" );
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public static void registerNewUser( User user ) {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         UserRepository userRepository = new UserRepository( em );
 
+        String hashedPassword = passwordEncoder.encode( user.getPassword() );
+        user.setPassword( hashedPassword );
         tx.begin();
         userRepository.create( user );
         tx.commit();
     }
+
+/*    public static void main( String[] args ) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode( "password" );
+        System.out.println( hashedPassword.length() );
+        System.out.println( hashedPassword );
+        System.out.println( encoder.matches( "passwor", hashedPassword ) );
+    }*/
+
 }
