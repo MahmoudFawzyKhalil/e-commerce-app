e771bca5df006114e4c9def2c18b104bdddfa937
Mariem <mariem.mfe@gmail.com>
2022-04-01 13:47:57 +0200

    Implement logout

---
+++ src/main/java/gov/iti/jets/presentation/controllers/UserLogoutControllerServlet.java
@@ -0,0 +1,37 @@
+package gov.iti.jets.presentation.controllers;
+
+import gov.iti.jets.presentation.util.CookieUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet( "/logout" )
+public class UserLogoutControllerServlet extends HttpServlet {
+
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+        doPost( request, response );
+
+    }
+
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+
+        Optional<Cookie> optionalRememberMeCookie = CookieUtil.getCookie( request.getCookies(), "rememberMeCookie" );
+        HttpSession session = request.getSession();
+
+        if ( optionalRememberMeCookie.isPresent() ) {
+            Cookie cookie = optionalRememberMeCookie.get();
+            cookie.setMaxAge( 0 );
+            response.addCookie( cookie );
+        }
+
+        session.invalidate();
+
+        response.sendRedirect( "home" );
+    }
+}
